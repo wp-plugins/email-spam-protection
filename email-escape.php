52cfb01977fb611fc66093cf55317ea7496e4bdf
ah125i <ah125i@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-15 17:25:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/email-spam-protection/trunk@65032 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-escape.php
+++ email-escape.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Email Spam Protection
 Plugin URI: http://blueberryware.net
-Description: Converts emails provided in the shortcode [escapeemail email="email@address.com"] to urlencoded javascript.  Based on email spam protection on Twitter's contact page. If you found the plugin helpful please rate it.  
+Description: Converts emails provided in the shortcode [escapeemail email="email@address.com"] to urlencoded javascript.  Based on email spam protection on Twitter's contact page. If you found the plugin helpful please rate it <a href="http://wordpress.org/extend/plugins/email-spam-protection/">here</a>.  
 
 Author: Adam Hunter
 Version: 1.0
